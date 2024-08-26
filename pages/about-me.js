786cec5810f9a7bbc8d0e573dd9b1c6adbdd94f4
junhek2525 <junhek2525@gmail.com>
2024-08-26 20:14:28 +0900

    8_26

--- pages/about-me.js
+++ pages/about-me.js
@@ -1,8 +1,10 @@
+import { Children } from "react";
 
 export default function AboutMe(){
     return(
-        <div>
+        <>
             <h1>자기소개가 들어갈 공간입니다.</h1>
-        </div>
+            <div>{Children}</div>
+        </>
     )
 }
---
+++ pages/projects.js
@@ -0,0 +1,8 @@
+export default function Projects()
+{
+    return(
+        <>
+            <h1>프로젝트</h1>
+        </>
+    )
+}
